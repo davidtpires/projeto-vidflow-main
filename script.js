2f38b530c221c0e0198b1e4f77603dae76b24325
David T. <davidt.info@gmail.com>
2024-12-11 15:02:30 -0300

    Construindo a lógica de pesquisa

--- script.js
+++ script.js
@@ -36,8 +36,18 @@ function filtrarPesquisa() {
     const videos = document.querySelectorAll(".videos__item");
 
     if(barraDePesquisa.value != ""){
-        
+        for(let video of videos) {
+            let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+            let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+            if(!titulo.includes(valorFiltro)){
+                video.style.display = "none";
+            } else {
+                video.style.display = "block";
+            }
+        }
     } else {
-        
+        video.style.display = "block";
+
     }
 }
