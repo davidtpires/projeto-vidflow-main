aedaec648fd8a5f6dce8d23e71acf1a4bcc297ef
David T. <davidt.info@gmail.com>
2024-12-11 15:29:45 -0300

    Criando a lógica de filtragem

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ async function buscarEMostrarVideos(params) {
                     <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
                     <h3 class="titulo-video">${video.titulo}</h3>
                     <p class="titulo-canal">${video.descricao}</p>
+                    <p class="categoria" hidden>${video.categoria}</p>
                 </div>
             </li>
             `;
@@ -61,4 +62,14 @@ botaoCategoria.forEach((botao) => {
 
 function filtrarPorCategoria(filtro) {
     const videos = document.querySelectorAll(".videos__item");
+    for (let video of videos) {
+        let categoria = video.querySelector(".categoria").textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
+
+        if(!categoria.includes(valorFiltro) && valorFiltro != 'tudo'){
+            video.style.display = "none";
+        } else {
+            video.style.display = "block";
+        }
+    }
 }
